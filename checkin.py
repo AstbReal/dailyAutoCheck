11d2bfe13f7a8db99285f37df6533b529183ccc1
AAANSU <86141997+AAANSU@users.noreply.github.com>
2022-03-08 12:53:33 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,36 +1,43 @@
-import json,requests,json,os
+import json
+import requests
+import json
+import os
 
 # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
 sever = os.environ["SERVE"]
+
 # 填写server酱sckey,不开启server酱则不用填
-sckey = os.environ["SCKEY"]
+sckey = os.environ["SERVER_SCKEY"]
+
 # 填入glados账号对应cookie
 cookie = os.environ["COOKIE"]
+
 # 企业微信的密钥
 wsecret = os.environ["WECHAT_SECRET"]
 
 
-def send_to_wecom(text,wecom_cid,wecom_aid,wecom_secret,wecom_touid='@all'):
+def send_to_wecom(text, wecom_cid, wecom_aid, wecom_secret, wecom_touid='@all'):
     get_token_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={wecom_cid}&corpsecret={wecom_secret}"
     response = requests.get(get_token_url).content
     access_token = json.loads(response).get('access_token')
     if access_token and len(access_token) > 0:
         send_msg_url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
         data = {
-            "touser":wecom_touid,
-            "agentid":wecom_aid,
-            "msgtype":"text",
-            "text":{
-                "content":text
+            "touser": wecom_touid,
+            "agentid": wecom_aid,
+            "msgtype": "text",
+            "text": {
+                "content": text
             },
-            "duplicate_check_interval":600
+            "duplicate_check_interval": 600
         }
-        response = requests.post(send_msg_url,data=json.dumps(data)).content
+        response = requests.post(send_msg_url, data=json.dumps(data)).content
         return response
     else:
         return False
 
-def send_to_wecom_image(base64_content,wecom_cid,wecom_aid,wecom_secret,wecom_touid='@all'):
+
+def send_to_wecom_image(base64_content, wecom_cid, wecom_aid, wecom_secret, wecom_touid='@all'):
     get_token_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={wecom_cid}&corpsecret={wecom_secret}"
     response = requests.get(get_token_url).content
     access_token = json.loads(response).get('access_token')
@@ -46,67 +53,77 @@ def send_to_wecom_image(base64_content,wecom_cid,wecom_aid,wecom_secret,wecom_to
 
         send_msg_url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
         data = {
-            "touser":wecom_touid,
-            "agentid":wecom_aid,
-            "msgtype":"image",
-            "image":{
+            "touser": wecom_touid,
+            "agentid": wecom_aid,
+            "msgtype": "image",
+            "image": {
                 "media_id": media_id
             },
-            "duplicate_check_interval":600
+            "duplicate_check_interval": 600
         }
-        response = requests.post(send_msg_url,data=json.dumps(data)).content
+        response = requests.post(send_msg_url, data=json.dumps(data)).content
         return response
     else:
         return False
 
-def send_to_wecom_markdown(text,wecom_cid,wecom_aid,wecom_secret,wecom_touid='@all'):
+
+def send_to_wecom_markdown(text, wecom_cid, wecom_aid, wecom_secret, wecom_touid='@all'):
     get_token_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={wecom_cid}&corpsecret={wecom_secret}"
     response = requests.get(get_token_url).content
     access_token = json.loads(response).get('access_token')
     if access_token and len(access_token) > 0:
         send_msg_url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
         data = {
-            "touser":wecom_touid,
-            "agentid":wecom_aid,
-            "msgtype":"markdown",
-            "markdown":{
-                "content":text
+            "touser": wecom_touid,
+            "agentid": wecom_aid,
+            "msgtype": "markdown",
+            "markdown": {
+                "content": text
             },
-            "duplicate_check_interval":600
+            "duplicate_check_interval": 600
         }
-        response = requests.post(send_msg_url,data=json.dumps(data)).content
+        response = requests.post(send_msg_url, data=json.dumps(data)).content
         return response
     else:
         return False
 
 
 def start():
-    url= "https://glados.rocks/api/user/checkin"
-    url2= "https://glados.rocks/api/user/status"
-    url3= "https://glados.rocks/api/user/traffic"
+    url = "https://glados.rocks/api/user/checkin"
+    url2 = "https://glados.rocks/api/user/status"
+    url3 = "https://glados.rocks/api/user/traffic"
     referer = 'https://glados.rocks/console/checkin'
     origin = "https://glados.rocks"
     useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-    payload={
+    payload = {
         'token': 'glados_network'
     }
-    checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
-    state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
-    traffic =  requests.get(url3,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+    checkin = requests.post(url, headers={'cookie': cookie, 'referer': referer, 'origin': origin,
+                            'user-agent': useragent, 'content-type': 'application/json;charset=UTF-8'}, data=json.dumps(payload))
+    state = requests.get(url2, headers={
+                         'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
+    traffic = requests.get(url3, headers={
+                           'cookie': cookie, 'referer': referer, 'origin': origin, 'user-agent': useragent})
     today = traffic.json()['data']['today']
     str = "cookie过期"
     if 'message' in checkin.text:
         mess = checkin.json()['message']
         time = state.json()['data']['leftDays']
         time = time.split('.')[0]
-        total = 200
+        total = 50
         use = today/1024/1024/1024
-        str = '[base] %s , you have %s days left. use: %.3f/%dGB(%.2f%%)' % (mess, time, use, total, use/total*100)
-    ret = send_to_wecom(str, "wwc216d22335b2bb48", "1000002", wsecret) # 换成自己的企业微信id
-    print(str, ret)
+        str = '%s , you have %s days left. use: %.3f/%dGB(%.2f%%)' % (mess, time, use, total, use/total*100)
+        ret = send_to_wecom(str, "wwc216d22335b2bb48","1000002", wsecret)  # 换成自己的企业微信id
+        print(str, ret)
+        if sever == 'on':
+            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?title=Glados_edu签到&desp=' + str )
+    else:
+        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?title=Glados_edu_cookie过期')
+
 
 def main_handler(event, context):
     return start()
+
 
 if __name__ == '__main__':
     start()
