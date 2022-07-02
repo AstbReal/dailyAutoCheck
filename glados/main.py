04a942c36fb30d7eaed50f11d893f5898fcd7f6b
ansu <zzesu@foxmail.com>
2022-07-02 14:14:43 +0800

    Update

--- glados/main.py
+++ glados/main.py
@@ -1,137 +1,13 @@
 # encoding=utf8
-from selenium.webdriver.support.ui import WebDriverWait
-import undetected_chromedriver as uc
 import io
-import requests
-import base64
 import os
 import sys
-# import time
-import json
-import subprocess
 from wecom import *
-# import glados.wecom as wecom
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+from checkin import *
 
-
-# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-sever = os.environ["SERVE"]
-
-# 填写server酱sckey,不开启server酱则不用填
-sckey = os.environ["SERVER_SCKEY"]
-
+# 若需要通知功能，请看wecom.py代码，此处只有cookie
 # 填入glados账号对应cookie
 cookie = os.environ["GLADOS_COOKIE"]
-
-# 企业微信的密钥
-wsecret = os.environ["WECHAT_SECRET"]
-
-# 企业ID
-wepid = os.environ["ENTERPRISE_ID"]
-
-# 应用ID
-appid = os.environ["APP_ID"]
-
-
-def get_driver_version():
-    cmd = r'''powershell -command "&{(Get-Item 'C:\Program Files\Google\Chrome\Application\chrome.exe').VersionInfo.ProductVersion}"'''
-    try:
-        out, err = subprocess.Popen(
-            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        out = out.decode('utf-8').split(".")[0]
-        return out
-    except IndexError as e:
-        print('Check chrome version failed:{}'.format(e))
-        return 0
-
-
-def get_checkin(driver):
-    checkin_url = "https://glados.rocks/api/user/checkin"
-    checkin_query = """
-        (function (){
-        var request = new XMLHttpRequest();
-        request.open("POST","%s",false);
-        request.setRequestHeader('content-type', 'application/json');
-        request.withCredentials=true;
-        request.send('{"token": "glados.network"}');
-        return request;
-        })();
-        """ % (checkin_url)
-    checkin_query = checkin_query.replace("\n", "")
-    resp_checkin = driver.execute_script("return " + checkin_query)
-    checkin = json.loads(resp_checkin["response"])
-    return checkin["code"], checkin["message"]
-
-
-def get_Status(driver):
-    status_url = "https://glados.rocks/api/user/status"
-    status_query = """
-        (function (){
-        var request = new XMLHttpRequest();
-        request.open("GET","%s",false);
-        request.send(null);
-        return request;
-        })();
-        """ % (status_url)
-    status_query = status_query.replace("\n", "")
-    resp = driver.execute_script("return " + status_query)
-    status = json.loads(resp["response"])
-    return status["data"]
-
-
-# def check_respcode(code):
-#     if code == -2:
-
-
-def glados(cookie_string):
-    options = uc.ChromeOptions()
-    options.add_argument("--disable-popup-blocking")
-
-    version = get_driver_version()
-    driver = uc.Chrome(version_main=version, options=options)
-
-    # Load cookie
-    driver.get("https://glados.rocks")
-
-    cookie_dict = [
-        {"name": x.split('=')[0].strip(), "value": x[x.find('=')+1:]}
-        for x in cookie_string.split(';')
-    ]
-
-    driver.delete_all_cookies()
-    for cookie in cookie_dict:
-        if cookie["name"] in ["koa:sess", "koa:sess.sig", "__stripe_mid", "__cf_bm"]:
-            driver.add_cookie({
-                "domain": "glados.rocks",
-                "name": cookie["name"],
-                "value": cookie["value"],
-                "path": "/",
-            })
-
-    driver.get("https://glados.rocks")
-    WebDriverWait(driver, 240).until(
-        lambda x: x.title != "Just a moment..."
-    )
-
-    checkin_code, checkin_message = get_checkin(driver)
-    if checkin_code == -2:
-        error = "Login Failed, cooike is invalid!"
-        print(error)
-        driver.close()
-        driver.quit()
-        return checkin_code
-    else:
-        if checkin_message != "Please Try Tomorrow":
-            status_message = get_Status(driver)
-            messages = [checkin_message, status_message]
-            message_notice(messages, True, wepid, appid, wsecret, sever, sckey)
-            success = "Checkin success!"
-            print(success)
-
-    driver.close()
-    driver.quit()
-
-    return checkin_code
 
 
 #  代码学习来自作者：[tyIceStream]https://github.com/tyIceStream/GLaDOS_Checkin.git
@@ -139,14 +15,13 @@ def glados(cookie_string):
 if __name__ == "__main__":
     # 支持多cookie签到,中间&&分隔开
     list_cookie = cookie.split("&&")
-    print("开始签到")
     for index, cookie in enumerate(list_cookie):
-        print("开始签到...")
-        resp_code = glados(cookie)
+        print(f"第{index+1}个账号正在签到...")
+        resp_code, message = glados(cookie)
         if resp_code == -2:
-            error = f"第{index+1}个账号cookie出现错误!请检查。"
-            message_notice(error, False, wepid, appid, wsecret, sever, sckey)
-            print(error)
-            continue
-        print(f"[第{index+1}个账号：签到成功!]")
-        # list_codes.append(resp_code)
+            message = f"第{index+1}个账号cookie出现错误!请检查。"
+            print(message)
+            message_notice(message, False)  # 发送成功消息给推动，并打印到终端。
+        else:
+            print(f"[第{index+1}个账号：签到成功!]")
+            message_notice(message, True)  # 发送失败消息给推送。
--- glados/wecom.py
+++ glados/wecom.py
@@ -1,6 +1,23 @@
 import base64
 import json
+import os
 import requests
+
+
+# server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
+sever = os.environ["SERVE"]
+
+# 填写server酱sckey,不开启server酱则不用填
+sckey = os.environ["SERVER_SCKEY"]
+
+# 企业微信的密钥
+wsecret = os.environ["WECHAT_SECRET"]
+
+# 企业ID
+wepid = os.environ["ENTERPRISE_ID"]
+
+# 应用ID
+appid = os.environ["APP_ID"]
 
 
 def send_to_wecom(text, wecom_cid, wecom_aid, wecom_secret, wecom_touid='@all'):
@@ -91,32 +108,34 @@ def limit_capacity(vip):
 
 
 # ok=true 则使用message通知具体内容; ok=false,则显示cookie登录失败
-def message_notice(message, ok, wepid, appid, wsecret, sever, sckey):
-    if ok:
-        # message = [checkin_message,status_message]
-        checkin = message[0]
-        status = message[1]
+def message_notice(message, ok):
 
-        mess = checkin
-        time = status['leftDays']
-        time = time.split('.')[0]
-        use = status['traffic']/1024/1024/1024  # float
-        capacity = limit_capacity(status['vip'])  # int
+    def notice():
+        if ok:
+            # message = [checkin_message,status_message]
+            checkin = message[0]
+            status = message[1]
 
-        use_rat = use/capacity*100  # ?
-        str_rat = '%.2f%%' % (use_rat)  # str
-        # print(type(mess), type(time), type(use), type(
-        # capacity), type(str_rat), type(use_rat))
-        wecomstr = '提示:%s; 目前剩余%s天; 流量已使用:%.3f/%dGB(%s)' % (
-            mess, time, use, capacity, str_rat)
-        # 换成自己的企业微信 idsend_to_wecom_image
-        ret = send_to_wecom(wecomstr, wepid, appid, wsecret)
-        print(wecomstr)
-        if sever == 'on':
-            send_to_sever(ok=True, message=wecomstr,
-                          sckey=sckey, mess=mess, time=time)
-    else:
-        # message = f"第{index+1}个账号cookie出现错误!请检查。"
-        send_to_wecom(message, wepid, appid, wsecret)
-        if sever == 'on':
-            send_to_sever(ok=False, message=message, sckey=sckey)
+            mess = checkin
+            time = status['leftDays']
+            time = time.split('.')[0]
+            use = status['traffic']/1024/1024/1024  
+            capacity = limit_capacity(status['vip'])  
+
+            use_rat = use/capacity*100  
+            str_rat = '%.2f%%' % (use_rat)
+
+            wecomstr = '提示:%s; 目前剩余%s天; 流量已使用:%.3f/%dGB(%s)' % (
+                mess, time, use, capacity, str_rat)
+            send_to_wecom(wecomstr, wepid, appid, wsecret) # 换成自己的企业微信 idsend_to_wecom_image
+            print(wecomstr)
+            if sever == 'on':
+                send_to_sever(ok=True, message=wecomstr,
+                              sckey=sckey, mess=mess, time=time)
+        else:
+            # message = f"第{index+1}个账号cookie出现错误!请检查。"
+            send_to_wecom(message, wepid, appid, wsecret)
+            if sever == 'on':
+                send_to_sever(ok=False, message=message, sckey=sckey)
+
+    notice()  # 闭包函数
